d19eba14405f27ef399ae02c69a8b6584615f166
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2025-09-15 11:51:35 +0200

    PoolingSystem optimization

--- GoGame3D/Assets/Scripts/Systems/PoolingSystem/PoolingSystem.cs
+++ GoGame3D/Assets/Scripts/Systems/PoolingSystem/PoolingSystem.cs
@@ -7,62 +7,63 @@ public class PoolingSystem : MonoBehaviour
     [SerializeField] private int _startSpawnCount = 5;
     [SerializeField] private int _spawnNewCount = 3;
 
-    private List<PoolableObject> _inactiveObjects;
-    private List<PoolableObject> _activeObjects;
+    private Queue<PoolableObject> _inactiveObjects;
+    private HashSet<PoolableObject> _activeObjects;
     private GameObject _poolableObjectsSocket;
 
     public void Awake()
     {
-        _inactiveObjects = new List<PoolableObject>();
-        _activeObjects = new List<PoolableObject>();
+        _inactiveObjects = new Queue<PoolableObject>();
+        _activeObjects = new HashSet<PoolableObject>();
+
         _poolableObjectsSocket = GameObject.Find("POOLABLE_OBJECTS_SOCKET");
         if (!_poolableObjectsSocket)
         {
-            _poolableObjectsSocket =  new GameObject("POOLABLE_OBJECTS_SOCKET");
+            _poolableObjectsSocket = new GameObject("POOLABLE_OBJECTS_SOCKET");
         }
+
         SpawnNew(_startSpawnCount);
     }
 
-    private PoolableObject SpawnNew(int count)
+    private void SpawnNew(int count)
     {
         for (int i = 0; i < count; i++)
         {
             PoolableObject newObject = Instantiate(_object, transform.position, transform.rotation)
                 .GetComponent<PoolableObject>();
+
             newObject.PoolingSystem = this;
             newObject.transform.parent = _poolableObjectsSocket.transform;
-            _inactiveObjects.Add(newObject);
+
             newObject.OnDespawn();
+            _inactiveObjects.Enqueue(newObject);
         }
-
-        return _inactiveObjects[0];
     }
-
+    
     public PoolableObject Spawn()
     {
-        PoolableObject selectedObject;
-        if (_inactiveObjects.Count > 0)
+        if (_inactiveObjects.Count == 0)
         {
-            selectedObject = _inactiveObjects[0];
+            SpawnNew(_spawnNewCount);
         }
-        else
-        {
-            selectedObject = SpawnNew(_spawnNewCount);
-        }
-        _inactiveObjects.Remove(selectedObject);
+
+        PoolableObject selectedObject = _inactiveObjects.Dequeue();
         _activeObjects.Add(selectedObject);
+
         selectedObject.transform.parent = null;
         selectedObject.OnSpawn();
+
         return selectedObject;
     }
-
     public void Despawn(PoolableObject objectToDespawn)
     {
         if (!_activeObjects.Contains(objectToDespawn)) return;
+
+        _activeObjects.Remove(objectToDespawn);
         objectToDespawn.transform.parent = _poolableObjectsSocket.transform;
         objectToDespawn.OnDespawn();
-        _inactiveObjects.Add(objectToDespawn);
-        _activeObjects.Remove(objectToDespawn);
+
+        _inactiveObjects.Enqueue(objectToDespawn);
     }
 
     private void OnDestroy()
